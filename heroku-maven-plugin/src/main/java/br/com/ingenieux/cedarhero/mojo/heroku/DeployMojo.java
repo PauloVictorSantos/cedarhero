e51028fbffd890af733329637326034712c30d6e
Aldrin Leal <aldrin@leal.eng.br>
2013-08-14 23:33:38 -0300

    Interim Commit

--- heroku-maven-plugin/src/main/java/br/com/ingenieux/cedarhero/mojo/heroku/DeployMojo.java
+++ heroku-maven-plugin/src/main/java/br/com/ingenieux/cedarhero/mojo/heroku/DeployMojo.java
@@ -1,59 +1,104 @@
 package br.com.ingenieux.cedarhero.mojo.heroku;
 
+import static org.apache.commons.lang.StringUtils.isBlank;
+
+import java.io.File;
+
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.errors.UnsupportedCredentialItem;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.lib.TextProgressMonitor;
+import org.eclipse.jgit.transport.CredentialItem;
 import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.CredentialsProviderUserInfo;
+import org.eclipse.jgit.transport.JschConfigSessionFactory;
+import org.eclipse.jgit.transport.OpenSshConfig;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.URIish;
 
-import java.io.File;
-
-import static org.apache.commons.lang.StringUtils.isBlank;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.UserInfo;
 
 @Mojo(name = "deploy")
 public class DeployMojo extends AbstractHerokuMojo {
-  /**
-   * Git Staging Dir (should not be under target/)
-   */
-  @Parameter(property = "heroku.stagingDirectory", defaultValue = "${project.basedir}/tmp-git-deployment-staging")
-  File stagingDirectory;
+	/**
+	 * Git Staging Dir (should not be under target/)
+	 */
+	@Parameter(property = "heroku.stagingDirectory", defaultValue = "${project.basedir}/tmp-git-deployment-staging")
+	File stagingDirectory;
 
-  /**
-   * The server id in maven settings.xml to use for AWS Services Credentials
-   * (accessKey / secretKey)
-   */
-  @Parameter(property = "heroku.sshKey", defaultValue = "${user.home}/.ssh/id_rsa")
-  protected String sshKey;
+	/**
+	 * The server id in maven settings.xml to use for AWS Services Credentials
+	 * (accessKey / secretKey)
+	 */
+	@Parameter(property = "heroku.sshKey", defaultValue = "${user.home}/.ssh/id_rsa")
+	protected String sshKey;
 
-  /**
-   * The App Name to push
-   */
-  @Parameter(property = "heroku.app")
-  protected String app;
+	/**
+	 * The App Name to push
+	 */
+	@Parameter(property = "heroku.app")
+	protected String app;
 
-  @Override
-  protected void executeInternal() throws Exception {
-    if (isBlank(app))
-      throw new MojoFailureException("app is not set");
+	@Override
+	protected void executeInternal() throws Exception {
+		if (isBlank(app))
+			throw new MojoFailureException("app is not set");
 
-    if (!stagingDirectory.exists())
-      throw new MojoFailureException("Inexistant staging directory. Create one with heroku:prepare");
+		if (!stagingDirectory.exists())
+			throw new MojoFailureException("Inexistant staging directory. Create one with heroku:prepare");
 
-    Git repo = Git.open(stagingDirectory);
+		Git repo = Git.open(stagingDirectory);
 
-    String remoteUrl = "ssh://git@heroku.com:" + app + ".git";
+		String remoteUrl = "git@heroku.com:" + app + ".git";
+		
+		StoredConfig config = repo.getRepository().getConfig();
+		
+		config.setString("remote", "heroku", "url", remoteUrl);
+		
+		config.save();
 
-    log("Pushing from %s to %s", stagingDirectory.getParent(), remoteUrl);
+		log("Pushing from %s to %s", stagingDirectory, remoteUrl);
 
-    PushCommand pushCommand = repo.push();
+		JschConfigSessionFactory sessionFactory = new JschConfigSessionFactory() {
+			@Override
+			protected void configure(OpenSshConfig.Host hc, Session session) {
+				session.setConfig("StrictHostKeyChecking", "no");
+				session.setConfig("IdentityFile", sshKey);
+				
+				CredentialsProvider provider = new CredentialsProvider() {
+					@Override
+					public boolean isInteractive() {
+						return false;
+					}
 
-    CredentialsProvider cp ;
-    pushCommand.setCredentialsProvider(cp);
+					@Override
+					public boolean supports(CredentialItem... items) {
+						return true;
+					}
 
-    pushCommand.setRemote(remoteUrl).add("master").setProgressMonitor(new TextProgressMonitor()).setForce(true).call();
+					@Override
+					public boolean get(URIish uri, CredentialItem... items) throws UnsupportedCredentialItem {
+						//for (CredentialItem item : items) {
+						//	((CredentialItem.StringType) item).setValue("yourpassphrase");
+						//}
+						return true;
+					}
+				};
+				UserInfo userInfo = new CredentialsProviderUserInfo(session, provider);
+				session.setUserInfo(userInfo);
+			}
+		};
+		SshSessionFactory.setInstance(sessionFactory);
 
-    //repo.push().setRemote("")
-  }
+		PushCommand pushCommand = repo.push().setRemote(remoteUrl).add("master").setProgressMonitor(new TextProgressMonitor()).setForce(true);
+				
+		pushCommand.call();
+
+		// repo.push().setRemote("")
+	}
 }
